9396de25d756ee4a6321f48d90b072ddbf7e81e7
Steven Williams <steven.williams12083@gmail.com>
2014-09-04 12:07:05 -0500

    Created my about page.

---
+++ StevenWilliams/Controllers/DemosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StevenWilliams.Controllers
+{
+    public class DemosController : Controller
+    {
+        // GET: Demos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
